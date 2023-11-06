e8288e60d6ed1f3bf7aff1704f2a459290778979
Tudor <128370090+psTudor@users.noreply.github.com>
2023-11-06 17:32:35 +0000

    file type

--- proiect.c
+++ proiect.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+typedef struct Img {
+    char numeFisier[20];
+    int inaltime;
+    int lungime;
+    int dimensiune;
+    char usrID[10];
+    char lastEdit[10];
+    int count;
+    char acUsr[3];
+    char acGrp[3];
+    char acAltii[3];
+}Img;
+
+char *getFileType(char *filename) {
+    char *dot = strrchr(filename, '.');
+    if (!dot || dot == filename) return "";
+    return dot + 1;
+}
+
 
 int main(int argc, char* argv[]) {
+    struct stat fisType;
+    if (argc != 2) perror("Numar de argumente gresit!");
+    stat(argv[1], &fisType);
+    printf("%F\n", fisType.st_mode);
     return 0;
 }
